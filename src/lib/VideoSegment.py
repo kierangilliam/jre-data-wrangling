40561c89785304153a530f334c5fd92215d78337
Kieran Gill <kieran.gill1997@gmail.com>
2020-11-18 10:06:06 -0800

    forgot to use fps divis

--- src/lib/VideoSegment.py
+++ src/lib/VideoSegment.py
@@ -45,7 +45,7 @@ def averages_for_video(filename, fps_divis, window=2500):
     fps_divis: terrible name but how many divisions of fps you want for averages. if fps is 30 and fps_divis is 2,
     grab 15 frames per. Larger the divis, the better the averaging will be for quick moves between frames, but the longer
     it will take
-    window: how many frames to process at one time (batch, only relates to how much RAM 
+    window: how many frames to process at one time (batch, only relates to how much RAM
     you're willing to use)
     """
     averages = []
@@ -56,7 +56,7 @@ def averages_for_video(filename, fps_divis, window=2500):
     fps = cap.get(cv2.CAP_PROP_FPS)
     while not done:
         frames, total, done = get_frames(
-            cap, window, skip=round(fps / 2), start=n * window
+            cap, window, skip=round(fps / fps_divis), start=n * window
         )
         averages.extend(get_averages(frames))
 
@@ -75,16 +75,17 @@ from sklearn.cluster import MeanShift, estimate_bandwidth, KMeans, DBSCAN
 from sklearn.neighbors.kde import KernelDensity
 from scipy.signal import argrelextrema
 
+
 def kde_cluster(Xdf, kernel="gaussian", bandwidth=10, plot=False):
     """
     Uses KDE to create clusters out of word vecs
     """
-    X = np.array(Xdf['x'])
+    X = np.array(Xdf["x"])
     X = X.reshape(-1, 1)
 
     # TODO There is probably bias at the boundaries, should mirror X
     kde = KernelDensity(kernel=kernel, bandwidth=bandwidth).fit(X)
-    s = np.linspace(0, np.max(X)*1.5)
+    s = np.linspace(0, np.max(X) * 1.5)
     e = kde.score_samples(s.reshape(-1, 1))
 
     # Reshape back to a 1 by N array
@@ -97,31 +98,31 @@ def kde_cluster(Xdf, kernel="gaussian", bandwidth=10, plot=False):
     minima_pairs = list(zip(np.insert(minima, 0, 0), np.append(minima, s[-1])))
 
     clusters = [
-      np.unique(X[np.logical_and(X >= m1, X < m2)]) for m1, m2 in minima_pairs
+        np.unique(X[np.logical_and(X >= m1, X < m2)]) for m1, m2 in minima_pairs
     ]
 
     if plot:
-      plt.plot(s, e)
-      plt.show()
-      print(f"Number of clusters: {len(clusters)}")
-      for c in clusters:
-          print("\t", len(c), np.unique([int(x) for x in c]))
+        plt.plot(s, e)
+        plt.show()
+        print(f"Number of clusters: {len(clusters)}")
+        for c in clusters:
+            print("\t", len(c), np.unique([int(x) for x in c]))
 
     return clusters
 
+
 # Store frame numbers in the clusters instead of the average of the last frame
 def frame_idxs_clusters(aavg_clusters, df):
-  df_2 = df_avg.copy()
-  df_2['cluster'] = np.full_like((len(averages)), -1)
+    df_2 = df_avg.copy()
+    df_2["cluster"] = np.full_like((len(averages)), -1)
 
-  # clusters = []
-  for i, c in enumerate(avg_clusters):
-      # clusters.append([])
-      for j, frame in enumerate(frames[:-1]):
-          if normal_averages[j] >= np.min(c) and normal_averages[j] <= np.max(c):
-              clusters[i].append(j)
-              df_2['cluster'][j] = i
-              continue
-  
-  return df_2
+    # clusters = []
+    for i, c in enumerate(avg_clusters):
+        # clusters.append([])
+        for j, frame in enumerate(frames[:-1]):
+            if normal_averages[j] >= np.min(c) and normal_averages[j] <= np.max(c):
+                clusters[i].append(j)
+                df_2["cluster"][j] = i
+                continue
 
+    return df_2
