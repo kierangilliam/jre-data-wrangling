73d732f2c2687a7515630b251d80f3bfdcbef480
Kieran Gill <kieran.gill1997@gmail.com>
2020-11-25 17:29:03 -0800

    tighten up how i do video segmenting

--- create_vid_avgs.py
+++ create_vid_avgs.py
@@ -7,24 +7,36 @@ import time
 import glob
 import os
 
+PROCS = 3
+videos_location = "/Volumes/JRE/jre-bucket/jre/videos/"
+SCALE = 0.25
+GRAYSCALE = False
+# TODO COMPARE SPEED WITH/WITHOUT GRAYSCALE
+# FPS_DIVIS = 15
+avg_cache = (
+    f"./data/jre/averages_scale.{SCALE*100}_"
+    + ("grayscale" if GRAYSCALE else "color")
+    + "/"
+)
+avgfn = lambda x: avg_cache + x.split(videos_location)[1].split(".mp4")[0]
 
-avg_cache = "./data/jre/averages_fps_divis_5/"
-avgfn = lambda x: avg_cache + x.split("../videos/")[1].split(".mp4")[0]
 
 def process_files(filenames):
     print(len(filenames))
 
     def process():
-        sys.stdout = open("avgs_" + str(os.getpid()) + ".out", "a")
-        sys.stderr = open(str(os.getpid()) + "_error.out", "a")
+        # sys.stdout = open("avgs_" + str(os.getpid()) + ".out", "a")
+        # sys.stderr = open(str(os.getpid()) + "_error.out", "a")
 
         for i, fn in enumerate(filenames):
             print(
                 f"{round(i/len(filenames),3)*100} - [{i}/{len(filenames)}]",
                 fn,
-                flush=True,
+                # flush=True,
             )
-            averages, total_frames = averages_for_video(fn, fps_divis=5)
+            averages, total_frames = averages_for_video(
+                fn, scale=SCALE, grayscale=GRAYSCALE
+            )
             np.save(avgfn(fn), [averages, total_frames])
 
     return process
@@ -37,10 +49,25 @@ def chunks(lst, n):
 
 
 if __name__ == "__main__":
-    PROCS = 8
+    if not os.path.exists(avg_cache):
+        print("Making directory", avg_cache)
+        os.makedirs(avg_cache)
 
-    files = list(glob.glob("../videos/*.mp4"))
+    print("SCALE", SCALE)
+    print("GRAYSCALE", GRAYSCALE)
+
+    files = list(glob.glob(videos_location + "*.mp4"))
     files = [f for f in files if not os.path.exists(avgfn(f) + ".npy")]
+
+    files = [
+        f
+        for f in files
+        if "#1539 - Jenny Kleeman" in f
+        or "#1538 - Douglas Murray" in f
+        or "#1536 - Edward Snowden" in f
+    ]
+    print(files)
+
     files = list(chunks(files, len(files) // PROCS))
     starttime = time.time()
     processes = []
--- src/lib/VideoSegment.py
+++ src/lib/VideoSegment.py
@@ -2,33 +2,38 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import pandas as pd
+import imutils
 
 
-def get_frames(cap, n=1000, start=120, skip=15):
+def get_frames(
+    cap, resize_to_width=None, window=1000, grayscale=False, skip=None, start=0
+):
     "`skip`: how many to skip between additions"
-    i = 0
     frames = []
     no_more_frames = False
 
-    while i < start:
-        cap.read()
-        i += 1
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start)
 
-    while len(frames) < n:
+    while len(frames) < window:
         ret, frame = cap.read()
 
         if not ret:
-            no_more_frames = True
-            break
+            return frames, True
+
+        if resize_to_width:
+            frame = imutils.resize(frame, width=resize_to_width)
+
+        if grayscale:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         frames.append(frame)
-        i += 1
 
-        for _ in range(skip):
-            i += 1
-            cap.read()
+        # TODO probably can do another cap.set(frame)
+        if skip:
+            for _ in range(skip):
+                cap.read()
 
-    return np.array(frames), i, no_more_frames
+    return frames, False
 
 
 def get_averages(frames):
@@ -40,7 +45,9 @@ def get_averages(frames):
 
 
 # Loop over entire video and compute averages for all of the frames
-def averages_for_video(filename, fps_divis, window=2500):
+def averages_for_video(
+    filename, grayscale=False, fps_divis=None, scale=None, window=2500
+):
     """
     fps_divis: terrible name but how many divisions of fps you want for averages. if fps is 30 and fps_divis is 2,
     grab 15 frames per. Larger the divis, the better the averaging will be for quick moves between frames, but the longer
@@ -49,80 +56,89 @@ def averages_for_video(filename, fps_divis, window=2500):
     you're willing to use)
     """
     averages = []
-    total_frames = n = 0
+    n = 0
     done = False
 
     cap = cv2.VideoCapture(filename)
     fps = cap.get(cv2.CAP_PROP_FPS)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    skip = None if fps_divis is None else round(fps / fps_divis)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) * scale) if scale else None
+
     while not done:
-        frames, total, done = get_frames(
-            cap, window, skip=round(fps / fps_divis), start=n * window
+        frames, done = get_frames(
+            cap,
+            window=window,
+            skip=skip,
+            start=n * window,
+            grayscale=grayscale,
+            resize_to_width=width,
         )
+
         averages.extend(get_averages(frames))
 
         frames = None
-        total_frames += total
         n += 1
 
-        print(f"\r\t{n * window}/{total_frames} frames")
+        print(f"{round(n * window / total_frames, 2)}% ({filename[-40:-16]})")
 
     cap.release()
 
     return np.array(averages), total_frames
 
 
-from sklearn.cluster import MeanShift, estimate_bandwidth, KMeans, DBSCAN
-from sklearn.neighbors.kde import KernelDensity
-from scipy.signal import argrelextrema
+# from sklearn.cluster import MeanShift, estimate_bandwidth, KMeans, DBSCAN
+# from sklearn.neighbors.kde import KernelDensity
+# from scipy.signal import argrelextrema
 
 
-def kde_cluster(Xdf, kernel="gaussian", bandwidth=10, plot=False):
-    """
-    Uses KDE to create clusters out of word vecs
-    """
-    X = np.array(Xdf["x"])
-    X = X.reshape(-1, 1)
+# def kde_cluster(Xdf, kernel="gaussian", bandwidth=10, plot=False):
+#     """
+#     Uses KDE to create clusters out of word vecs
+#     """
+#     X = np.array(Xdf["x"])
+#     X = X.reshape(-1, 1)
 
-    # TODO There is probably bias at the boundaries, should mirror X
-    kde = KernelDensity(kernel=kernel, bandwidth=bandwidth).fit(X)
-    s = np.linspace(0, np.max(X) * 1.5)
-    e = kde.score_samples(s.reshape(-1, 1))
+#     # TODO There is probably bias at the boundaries, should mirror X
+#     kde = KernelDensity(kernel=kernel, bandwidth=bandwidth).fit(X)
+#     s = np.linspace(0, np.max(X) * 1.5)
+#     e = kde.score_samples(s.reshape(-1, 1))
 
-    # Reshape back to a 1 by N array
-    X = X.reshape(1, -1)
+#     # Reshape back to a 1 by N array
+#     X = X.reshape(1, -1)
 
-    minima = argrelextrema(e, np.less)[0]
-    # Use the linspace to convert back into word indexes
-    minima = [s[m] for m in minima]
-    # (0, minima 1), (minima 1, minima 2), ... (minima n-1, minima n), (minima n, end)
-    minima_pairs = list(zip(np.insert(minima, 0, 0), np.append(minima, s[-1])))
+#     minima = argrelextrema(e, np.less)[0]
+#     # Use the linspace to convert back into word indexes
+#     minima = [s[m] for m in minima]
+#     # (0, minima 1), (minima 1, minima 2), ... (minima n-1, minima n), (minima n, end)
+#     minima_pairs = list(zip(np.insert(minima, 0, 0), np.append(minima, s[-1])))
 
-    clusters = [
-        np.unique(X[np.logical_and(X >= m1, X < m2)]) for m1, m2 in minima_pairs
-    ]
+#     clusters = [
+#         np.unique(X[np.logical_and(X >= m1, X < m2)]) for m1, m2 in minima_pairs
+#     ]
 
-    if plot:
-        plt.plot(s, e)
-        plt.show()
-        print(f"Number of clusters: {len(clusters)}")
-        for c in clusters:
-            print("\t", len(c), np.unique([int(x) for x in c]))
+#     if plot:
+#         plt.plot(s, e)
+#         plt.show()
+#         print(f"Number of clusters: {len(clusters)}")
+#         for c in clusters:
+#             print("\t", len(c), np.unique([int(x) for x in c]))
 
-    return clusters
+#     return clusters
 
 
-# Store frame numbers in the clusters instead of the average of the last frame
-def frame_idxs_clusters(aavg_clusters, df):
-    df_2 = df_avg.copy()
-    df_2["cluster"] = np.full_like((len(averages)), -1)
+# # Store frame numbers in the clusters instead of the average of the last frame
+# def frame_idxs_clusters(aavg_clusters, df):
+#     df_2 = df_avg.copy()
+#     df_2["cluster"] = np.full_like((len(averages)), -1)
 
-    # clusters = []
-    for i, c in enumerate(avg_clusters):
-        # clusters.append([])
-        for j, frame in enumerate(frames[:-1]):
-            if normal_averages[j] >= np.min(c) and normal_averages[j] <= np.max(c):
-                clusters[i].append(j)
-                df_2["cluster"][j] = i
-                continue
+#     # clusters = []
+#     for i, c in enumerate(avg_clusters):
+#         # clusters.append([])
+#         for j, frame in enumerate(frames[:-1]):
+#             if normal_averages[j] >= np.min(c) and normal_averages[j] <= np.max(c):
+#                 clusters[i].append(j)
+#                 df_2["cluster"][j] = i
+#                 continue
 
-    return df_2
+#     return df_2
